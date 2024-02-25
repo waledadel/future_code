8f67d0ed2abd661e3df10e782aa7499f2bdd9b79
Waleed Adel <wadel@jiosdev.com>
2024-02-26 00:19:22 +0200

    Prepare Lesson 22

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 
@@ -9,16 +10,19 @@ import { CourseCardComponent } from './course-card/course-card.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CourseCardComponent],
+  imports: [RouterOutlet, CourseCardComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
 
   data = {
-    courseName: 'Angular Core Course!',
-    lessonName: '21- Angular Built-In Pipes Part 1 - Transforming Data Using Pipes'
+    courseName: 'angular core Course!',
+    lessonName: '22- Angular Built-In Pipes Part 2 - Transforming Data Using Pipes'
   };
+
+  date = new Date();
+  amount = 50.044444444444444444;
 
   readonly courses: Array<ICourse> = courses;
 
